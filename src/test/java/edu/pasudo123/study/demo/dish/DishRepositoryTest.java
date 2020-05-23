d804c9e51f9749ee1334bbf8e01a505a9c0490de
PARK SUNG DONG <oraedoa@gmail.com>
2020-05-23 20:29:35 +0900

    [test] adding ã테 @Sql Annotation

--- src/test/java/edu/pasudo123/study/demo/dish/DishRepositoryTest.java
+++ src/test/java/edu/pasudo123/study/demo/dish/DishRepositoryTest.java
@@ -1,15 +1,39 @@
 package edu.pasudo123.study.demo.dish;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
+@Sql(scripts = {"classpath:data/dish_insert.sql"})
+@ActiveProfiles("test")
 @DisplayName("요리 레파지토리는")
 class DishRepositoryTest {
 
+    @Autowired
+    private TestEntityManager testEntityManager;
 
+    @Autowired
+    private DishRepository dishRepository;
 
+    @Test
+    @DisplayName("전체 요리를 조회한다.")
+    public void Should_DishesSizeGreaterThanZero_When_FindAll(){
+        // when
+        final List<Dish> dishes = dishRepository.findAll();
+
+        // then
+        assertThat(dishes).hasSizeGreaterThan(0);
+    }
 }
